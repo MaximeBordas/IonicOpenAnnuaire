ffc8eed51375c9d485886394bc9a43b0e30ec4d5
MaximeBordas <maxime.bordas01@gmail.com>
2018-03-11 20:25:27 +0100

    Amen la map s'affiche et fonctionne mais le style je laisse tomber

--- src/pages/mapCompanies/mapCompanies.ts
+++ src/pages/mapCompanies/mapCompanies.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { Company } from "../../app/Models/company";
 import { RetrieveCompaniesService } from "../../app/retrieve-companies.service";
+import  { mapStyle} from "./mapStyle"
 
 declare let google: any;
 
@@ -12,6 +13,10 @@ export class mapCompanies implements OnInit {
   @ViewChild('mapCompanies') mapRef: ElementRef;
   companies: Company[];
   alreadyFiltered = false;
+  style = [];
+
+  style = mapStyle;
+
 
   constructor(private retrieveCompaniesService: RetrieveCompaniesService) {
     this.retrieveCompaniesService.filterCompanies.subscribe(
@@ -34,15 +39,15 @@ export class mapCompanies implements OnInit {
 
     const options = {
       center: location,
-      zoom: 8,
+      zoom: 6,
     };
 
-    const map = new google.maps.Map(this.mapRef.nativeElement, options);
+    const map = new google.maps.Map(this.mapRef.nativeElement, options,{styles : this.style});
 
     this.companies.forEach((company: Company) => {
       if (undefined !== company.coordonnees) {
         let latLng = new google.maps.LatLng(company.coordonnees[0], company.coordonnees[1]);
-        let content = "Nom : " + company.name + "<hr>Adresse : " + company.address + "<br>Code postal : " + company.postal_code + "<br>Ville : " + company.city;
+        let content = "Nom : " + company.name + "<hr>Adresse : " + company.address + "<br>Code postal : " + company.postal_code + "<br>Ville de l'entreprise : " + company.city;
         let infoWindow = this.infoWindow(content);
         let marker = this.addMarker(latLng, map, company.name);
 
