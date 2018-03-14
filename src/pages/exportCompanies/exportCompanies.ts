ecc7e5f603531e163db39181bcc28bd124b9591f
MaximeBordas <maxime.bordas01@gmail.com>
2018-03-14 13:31:10 +0100

    lexport est de type bancal mais EH ça fonctionne (Pas sur mobile mais qui dl ça sur mobile sérieux?)

--- src/pages/exportCompanies/exportCompanies.ts
+++ src/pages/exportCompanies/exportCompanies.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { InAppBrowser} from "@ionic-native/in-app-browser";
 
 @Component({
   selector: 'page-export',
-  templateUrl: 'exportCompanies.html'
+  templateUrl: 'exportCompanies.html',
+
 })
 export class exportCompanies {
 
-  constructor(public navCtrl: NavController) {
+   CSV = 'https://public.opendatasoft.com/explore/dataset/sirene/download/?format=csv&use_labels_for_header=true';
+   JSON = 'https://public.opendatasoft.com/explore/dataset/sirene/download/?format=json&use_labels_for_header=true';
+   XLS = 'https://public.opendatasoft.com/explore/dataset/sirene/download/?format=xls&use_labels_for_header=true';
+   url: string;
+  constructor(private iab: InAppBrowser) {
 
   }
 
+
+  downLoadAPI(typeExport: string){
+    switch (typeExport){
+      case 'CSV':
+        this.url = this.CSV;
+        break;
+      case 'JSON':
+        this.url = this.JSON;
+        break;
+      case 'XLS':
+        this.url = this.XLS;
+        break;
+    }
+    const navigateur = this.iab.create(this.url);
+    navigateur.show();
+  }
 }
