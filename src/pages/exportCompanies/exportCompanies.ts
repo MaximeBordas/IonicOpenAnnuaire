d539a873e24d565fdede6ed1b8fe613db1449da3
MaximeBordas <maxime.bordas01@gmail.com>
2018-03-15 14:25:13 +0100

    Add export mobile

--- src/pages/exportCompanies/exportCompanies.ts
+++ src/pages/exportCompanies/exportCompanies.ts
@@ -1,22 +1,107 @@
 import { Component } from '@angular/core';
+import { File } from "@ionic-native/file";
+import {Transfer, TransferObject} from '@ionic-native/transfer';
+import { AlertController, LoadingController,Platform } from 'ionic-angular';
 import { InAppBrowser} from "@ionic-native/in-app-browser";
+
+
+declare var cordova: any;
 
 @Component({
   selector: 'page-export',
   templateUrl: 'exportCompanies.html',
+  providers: [Transfer,TransferObject,File]
 
 })
 export class exportCompanies {
 
+  url: string;
+  path: string ='';
    CSV = 'https://public.opendatasoft.com/explore/dataset/sirene/download/?format=csv&use_labels_for_header=true';
    JSON = 'https://public.opendatasoft.com/explore/dataset/sirene/download/?format=json&use_labels_for_header=true';
    XLS = 'https://public.opendatasoft.com/explore/dataset/sirene/download/?format=xls&use_labels_for_header=true';
-   url: string;
-  constructor(private iab: InAppBrowser) {
+  constructor(private iab: InAppBrowser,private platform: Platform, private transfer: Transfer,private alertCtrl: AlertController,private loadingCtrl: LoadingController) {
 
+    if(this.platform.is('ios')){
+      this.path = cordova.file.documentsDirectory;
+    }
+    if(this.platform.is('android')){
+      this.path = cordova.file.externalDataDirectory;
+    }
   }
 
+  DownloadAll(format: string)
+  {
+    let loading = this.loadingCtrl.create({
+      content: 'Téléchargement en cours...'
+    });
 
+    const fileTransfert: TransferObject = this.transfer.create();
+
+    switch (format){
+      case 'CSV':
+        fileTransfert.download(this.CSV,this.path).then((entry) => {
+            const alertSuccess = this.alertCtrl.create({
+              title: `Fichier ${format} téléchargé.`,
+              subTitle: `Le fichier .${format} à bien été téléchargé dans le path : ${entry.toURL()}`,
+              buttons: ['Ok']
+            });
+
+            loading.dismiss();
+            alertSuccess.present();
+          }, (error) => {
+          const alertFailure = this.alertCtrl.create({
+            title: `Download Failed!`,
+            subTitle: `Erreur lors du téléchargement. Error code: ${error.code}`,
+            buttons: ['Ok']
+          });
+          loading.dismiss();
+          alertFailure.present();
+          });
+
+        break;
+        case 'JSON':
+          fileTransfert.download(this.JSON,this.path).then((entry) => {
+          const alertSuccess = this.alertCtrl.create({
+            title: `Fichier ${format} téléchargé.`,
+            subTitle: `Le fichier .${format} à bien été téléchargé dans le path : ${entry.toURL()}`,
+            buttons: ['Ok']
+          });
+
+          loading.dismiss();
+          alertSuccess.present();
+        }, (error) => {
+          const alertFailure = this.alertCtrl.create({
+            title: `Download Failed!`,
+            subTitle: `Erreur lors du téléchargement. Error code: ${error.code}`,
+            buttons: ['Ok']
+          });
+          loading.dismiss();
+          alertFailure.present();
+        });
+          break;
+        case 'XLS':
+          fileTransfert.download(this.XLS,this.path).then((entry) => {
+          const alertSuccess = this.alertCtrl.create({
+            title: `Fichier ${format} téléchargé.`,
+            subTitle: `Le fichier .${format} à bien été téléchargé dans le path : ${entry.toURL()}`,
+            buttons: ['Ok']
+          });
+
+          loading.dismiss();
+          alertSuccess.present();
+        }, (error) => {
+          const alertFailure = this.alertCtrl.create({
+            title: `Download Failed!`,
+            subTitle: `Erreur lors du téléchargement. Error code: ${error.code}`,
+            buttons: ['Ok']
+          });
+          loading.dismiss();
+          alertFailure.present();
+        });
+          break;
+    }
+  }
   downLoadAPI(typeExport: string){
     switch (typeExport){
       case 'CSV':
